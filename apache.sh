a73069fd3943bf9b4bf75b08954c9ecb09c9f7d9
Sarath Racherla <sharathsaee@gmail.com>
2019-10-10 23:51:10 +0530

    11:50PMIST-10Oct19-Added AMI along with Custom Scripts
    11:50PMIST-10Oct19-Added AMI along with Custom Scripts

--- apache.sh
+++ apache.sh
@@ -1,3 +1,5 @@
+#AMI Used is Amazon Linux
+
 #!bin/bash
 sudo su
 cd
@@ -5,3 +7,18 @@ yum update -y
 yum install httpd -y
 service httpd start
 chkconfig httpd on
+cd /var/www/html
+git clone https://github.com/vCloudmateguru/ion.git
+
+
+#AMI Used is Ubuntu Linux
+
+#!bin/bash
+sudo su
+cd
+apt-get update -y
+apt-get install apache2 -y
+service apache2 start
+chkconfig apache2 on
+cd /var/www/html
+git clone https://github.com/vCloudmateguru/ion.git
